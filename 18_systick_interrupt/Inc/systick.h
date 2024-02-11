d0a5e844298c52aca27fb3db8e7bbb99bceed3dd
sanjay <imcoolsaka@rediff.com>
2024-02-11 17:27:55 +0530

    added interrupt project

---
+++ 18_systick_interrupt/Inc/systick.h
@@ -0,0 +1,13 @@
+/*
+ * systick.h
+ *
+ *  Created on: Jan 23, 2024
+ *      Author: sanjay.kumar
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+void systickDelayMs(int delay);
+void systick1HzInterruptInit();
+
+#endif /* SYSTICK_H_ */
---
+++ 19_Timers_Interrupt/Inc/tim.h
@@ -0,0 +1,14 @@
+/*
+ * tim.h
+ *
+ *  Created on: Jan 24, 2024
+ *      Author: sanjay.kumar
+ */
+
+#ifndef TIM_H_
+#define TIM_H_
+
+#define UIF		(1U<<0)
+void timer6_init1Hz();
+void timer6_init1HzInterrupt();
+#endif /* TIM_H_ */
